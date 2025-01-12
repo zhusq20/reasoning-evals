82f4ac39ec00e286c07f5c0de3ca1418b027aab5
siqi zhu <zhusq20@gmail.com>
2025-01-12 10:51:19 +0000

    change readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # reasoning-evals
+
+## MMLU, GPQA, HumanEval, MGSM, DROP
+
+follow instructions in ./simple-evals/run.sh
+
+## AIME, MATH500, GSM8K
+
+follow the instructions in ./Qwen2.5-Math/evaluation/README.md
+
+## ARC-AGI
+
+working on
+
+## SWE-bench Verified
+
+working on
--- simple-evals/run.sh
+++ simple-evals/run.sh
@@ -1,13 +1,18 @@
+# 1.首先启动待评测的LLM
 vllm serve /workspace/HuggingFace-Download-Accelerator/qwen25/models--Qwen--Qwen2.5-7B-Instruct
 
-# 2.在simple-evals的父目录运行:
+# 2.更改设置
+'''
+simple_evals.py需要修改的地方:
+
+line 36: models 里面加你要评测的模型
+
+line 113: grading_sampler = models["qwen2.5-7b"]和
+line 114: equality_checker = models["qwen2.5-7b"]改成你想用的模型，
+原本的代码用的是gpt-4o
+
+line 156: 设置你想评测的数据集
+'''
+
+# 3.在simple-evals的父目录(根目录)运行:
 python -m simple-evals.simple_evals --model qwen2.5-7b --debug
-
-
-'''
-simple_evals.py需要修改的地方
-line36: models = 后面加新的模型
-line113: grading_sampler = models["qwen2.5-7b"]
-line114: equality_checker = models["qwen2.5-7b"]改成新的模型名称
-
-'''
